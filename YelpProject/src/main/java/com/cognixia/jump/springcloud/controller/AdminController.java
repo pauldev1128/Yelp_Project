2e9129840a4cd45e4d46d1598253c98f7a631013
Andreas-Wall <m.andreas.wall@protonmail.com>
2021-06-30 14:53:42 -0500

    added admin

--- YelpProject/src/main/java/com/cognixia/jump/springcloud/controller/AdminController.java
+++ YelpProject/src/main/java/com/cognixia/jump/springcloud/controller/AdminController.java
@@ -1,5 +1,36 @@
 package com.cognixia.jump.springcloud.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognixia.jump.springcloud.repository.AdminRepository;
+
+@CrossOrigin
+@RequestMapping("/api")
+@RestController
 public class AdminController {
 
+	@Autowired
+	AdminRepository service;
+	
+	@CrossOrigin
+	@PatchMapping("/update/Resturant")
+	public @ResponseBody String updateResturant(@RequestBody Resturant rest) {
+		Optional<Resturant> found = service.findAllById(rest.getID());
+		
+		if(found.isPresent()) {
+			service.save(rest);
+			return "Saved: " + rest.toString();
+		}
+		else {
+			return "Could not update resturant, the id = " + rest.getId() + " doesn't exist";
+		}
+	}
 }
---
+++ YelpProject/src/main/java/com/cognixia/jump/springcloud/repository/AdminRepository.java
@@ -0,0 +1,14 @@
+package com.cognixia.jump.springcloud.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cognixia.jump.springcloud.model.Admin;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+	List<Admin> findAll();
+}
